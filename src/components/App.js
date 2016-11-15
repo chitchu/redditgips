702978d7794c5f752af160a67008c234831c821e
Vicente Jr Yuchitcho <chitchu@gmail.com>
2016-11-15 15:05:24 +1100

    Update card style

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import Cards from './Cards';
 import 'normalize.css';
-// import SvgIcon from 'material-ui/'
-// import Snoo from '../sharing-reddit.svg';
-
 
 class App extends Component {
   render() {
