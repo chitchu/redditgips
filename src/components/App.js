76ba0647770f2aebcc532d0c194997fc0470333b
Vicente Jr Yuchitcho <chitchu@gmail.com>
2016-12-02 12:12:51 +1100

    Move <Root/> code to <App/>

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,36 @@
 import AppBar from 'material-ui/AppBar';
-import Cards from './Cards';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React, { Component } from 'react';
+import Thunk from 'redux-thunk';
+
+import Cards from './Cards';
+import Reducers from '../ducks/';
+
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+//material-ui requirement
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 import 'normalize.css';
+
+const store = createStore(
+  Reducers,
+  applyMiddleware(Thunk)
+);
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <AppBar title="redditgips" />
-        <Cards/>
-      </div>
+      <Provider store={store}>
+        <MuiThemeProvider>
+          <div>
+            <AppBar title="redditgips" />
+            <Cards/>
+          </div>
+        </MuiThemeProvider>
+      </Provider>
     );
   }
 }
