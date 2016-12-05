e1db7cc3b72b247de305f2d33b397c475e3da63f
Vicente Jr Yuchitcho <chitchu@gmail.com>
2016-12-05 13:08:11 +1100

    Giphy fix

--- src/components/Gipher.js
+++ src/components/Gipher.js
@@ -5,12 +5,15 @@ const Gipher = (props) => {
   const StyledVideo = styled.video`
     position: relative;
     width: 100%;
+    display: block;
   `;
 
   const StyledImg = styled.img`
     position: relative;
     width: 100%;
+    display: block;
   `;
+
   const { domain, url, thumbnail } = props;
 
   let element;
@@ -19,7 +22,7 @@ const Gipher = (props) => {
     case 'i.imgur.com':
       const imgurname = url.split('/')[url.split('/').length -1].split('.')[0];
       element = (
-        <StyledVideo autoPlay loop>
+        <StyledVideo loop>
             <source src={`//i.imgur.com/${imgurname}.mp4`} type="video/mp4" />
         </StyledVideo>
       );
@@ -27,7 +30,7 @@ const Gipher = (props) => {
     case 'gfycat.com':
       const name = url.split('/')[url.split('/').length - 1];
       element = (
-        <StyledVideo autoPlay loop>
+        <StyledVideo loop>
           <source id="webmSource" src={`https://zippy.gfycat.com/${name}.webm`} type="video/webm" />
           <source id="webmSource" src={`https://fat.gfycat.com/${name}.webm`} type="video/webm" />
           <source id="mp4Source" src={`https://zippy.gfycat.com/${name}.mp4`} type="video/mp4" />
@@ -48,9 +51,11 @@ const Gipher = (props) => {
       break;
     case 'media.giphy.com':
     default:
-        element = (
-          <StyledImg src={url} role="presentation" />
-        );
+      let giphyUrl = (url.match('.gif')) ? url : `https://media.giphy.com/media/${url.substr(url.lastIndexOf('-') + 1)}/giphy.gif`;
+      console.log (url, giphyUrl);
+      element = (
+        <StyledImg src={giphyUrl} role="presentation" />
+      );
       break;
   }
   return element;
