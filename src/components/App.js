b65479fef5a93866ddd68212db357a055828f463
Vicente Jr Yuchitcho <chitchu@gmail.com>
2016-12-07 22:44:45 +1100

    Remove debug tools

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 const store = createStore(Reducers
-  , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   , applyMiddleware(Thunk)
 );
 
