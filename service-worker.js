38a83e1899ed4343ba7ad53e62d3e85b7e914dd3
Vicente Jr Yuchitcho <chitchu@gmail.com>
2016-12-15 16:39:51 +1100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","88a32beaf0a87de3520f6f22439460af"],["manifest.json","9cc318ac89498dfb4ad83e156020e14a"],["static/css/main.7daa6915.css","2541d8a0955b841aeb333bd60d8e4061"],["static/js/main.75d7b2a8.js","72245b33b8fac3969d6d435e4c79c582"],["static/media/ic_signal_wifi_off_black_24px.ac21f916.svg","ac21f916943249882e708be73be94c3a"]];
+var precacheConfig = [["index.html","a8ce7239b9060732ed2142bd69b5264e"],["manifest.json","9cc318ac89498dfb4ad83e156020e14a"],["static/css/main.7daa6915.css","2541d8a0955b841aeb333bd60d8e4061"],["static/js/main.d5c6d3cc.js","c4592e648efc089f0b7c711b2d3dc04e"],["static/media/ic_signal_wifi_off_black_24px.ac21f916.svg","ac21f916943249882e708be73be94c3a"]];
 var cacheName = 'sw-precache-v2-sw-precache-' + (self.registration ? self.registration.scope : '');
 
 
